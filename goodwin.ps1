a7419ce7fffc09ce406258d9ee3b031b006ca011
Elijah <ewhw@pm.me>
2022-05-31 03:32:36 +0200

    Update goodwin.ps1

--- goodwin.ps1
+++ goodwin.ps1
@@ -2,5 +2,5 @@ $InstallDir='C:\ProgramData\chocoportable'
 $env:ChocolateyInstall="$InstallDir"
 $ISO1="scripts\controller.ps1"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
-choco feature disable -n allowGlobalConfirmation
+choco feature enable -n allowGlobalConfirmation
 & "$PSScriptRoot\$ISO1"
