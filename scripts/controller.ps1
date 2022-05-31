59303bc629b9d41831ab9a5c302f5b4ca8c605bb
Elijah <ewhw@pm.me>
2022-05-31 03:27:53 +0200

    update

--- scripts/controller.ps1
+++ scripts/controller.ps1
@@ -1,7 +1,7 @@
 # Installation script for Elijah Basic Windows Applications
 ## Variables
 $p = "packages\"
-$i = "sudo choco install"
+$i = "choco install"
 $z = "-fy"
 $c = ".config"
 
@@ -10,94 +10,112 @@ echo "Initalizing Applications..."
 $packList = Get-ChildItem ".\packages" | ForEach-Object{($_ -replace(".config", ""))}
 Start-Sleep -s 2
 
+# INTRO # 
 echo "There are multiple categories to install packages from, categories include:"
 echo $packList
 echo "~~~~~~~~~~"
 $response = read-host "Write the name of which packet cagetory you want to install first"
 
-# ESSENTIALS #
-echo "Essentials category include the following programs:"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~"
-Get-Content $p$response$c
-$responseEssentials = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
-if($responseEssentials -eq "y") {
+# ESSENTIALS - START #
+    echo "Essentials include the following programs:"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseEssentials = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseEssentials -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+        } else {echo "Skipped Essentials.."}
+# ESSENTIALS - END #
 
- Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+# DEVELOPMENT - START #
+    echo "Development include the following programs:"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseDevelopment = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseDevelopment -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+        } else {echo "Skipped Development.."}
+# DEVELOPMENT - END #
+
+# BROWSERS - START #
+    echo "Browsers include the following programs:"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseBrowsers = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseBrowsers -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+        } else {echo "Skipped Browsers.."}
+# BROWSERS - END #
+
+# ENTERTAINMENT - START #
+    echo "Entertainment include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseEntertainment = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseEntertainment -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+        } else {echo "Skipped Entertainment.."}
+# ENTERTAINMENT - END #
+
+# FORENSICS - START #
+    echo "Forensics include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseForensics = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseForensics -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+        } else {echo "Skipped Forensics.."}
+# FORENSICS - END #
+
+# PREMA - START #
+    echo "Prema include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responsePrema = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responsePrema -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+    } else {echo "Skipped Prema.."}
+# PREMA - END #
+
+# COMMS - START #
+    echo "Comms include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseComms = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseComms -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+    } else {echo "Skipped Comms.."}
+# COMMS - END #
+
+# SYSTEM - START #
+    echo "System include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseSystem = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseSystem -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+    } else {echo "Skipped System.."}
+# SYSTEM - END #
  
-} else {echo "Skipped Essentials.."}
-break;
+# OTHER - START #
+    echo "Other include the following programs:"     
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        Get-Content $p$response$c
+        $responseOther = read-host "Do you want to install the listed programs? ([Y]es/[N]o)"
+        if($responseOther -eq "y")
+        {
+        Get-Content $p$response$c | ForEach-Object{$i + " " + $_ + " " +$z} | iex
+    } else {echo "Skipped Other.."}
+# OTHER - END #
 
+# CONFIGURATION #
 
-
-
-$Development = read-host "Do you want to install the Development Pack? ([Y]es/[N]o)"
-if($Development -eq "y") {
-# DEVELOPMENT #
-
-
-} else {echo "Skipped Development.."}
-
-
-$Browsers = read-host "Do you want to install the Browsers Pack? ([Y]es/[N]o)"
-if($Browsers -eq "y") {
-# BROWSERS #
-
-} else {echo "Skipped Browsers.."}
-
-$Entertainment = read-host "Do you want to install the Entertainment Pack? ([Y]es/[N]o)"
-if($Entertainment -eq "y") {
-# ENTERTAINMENT #
-
-
-} else {echo "Skipped Entertainment.."}
-
-
-$Forensics = read-host "Do you want to install the Forensics Pack? ([Y]es/[N]o)"
-if($Forensics -eq "y") {
-# FORENSICS #
-
-
-} else {echo "Skipped Forensics.."}
-
-echo
-$ReverseEngineering = read-host "Do you want to install the ReverseEngineering Pack? ([Y]es/[N]o)"
-if($ReverseEngineering -eq "y") {
-
-# REVERSE ENGINEERING #
-
-
-} else {echo "Skipped ReverseEngineering.."}
-
-
-$Communications = read-host "Do you want to install the Communications Pack? ([Y]es/[N]o)"
-if($Communications -eq "y") {
-
-# COMMUNICATIONS #
-
-
-} else {echo "Skipped Communications.."}
-
-
-$System = read-host "Do you want to install the System Pack? ([Y]es/[N]o)"
-if($System -eq "y") {
-
-# SYSTEM #
-
-
-} else {echo "Skipped System.."}
-
-Start-Sleep -s 2
-$Other = read-host "Do you want to install the Other Pack? ([Y]es/[N]o)"
-if($Other -eq "y") {
-
-# OTHERS #
-
-
-} else {echo "Skipped Other.."}
-
-
-
-# OTHER CONFIGURATION #
-#choco feature disable -n allowGlobalConfirmation
 
 break;
